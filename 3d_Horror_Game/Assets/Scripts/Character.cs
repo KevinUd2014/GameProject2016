f15d80aa8be7115f9525827d022e21e36df934a6
KevinUd2014 <kn222gn@student.lnu.se>
2016-03-31 21:43:48 +0200

    now uploading last test commit for today, good night

--- 3d_Horror_Game/Assets/Scripts/Character.cs
+++ 3d_Horror_Game/Assets/Scripts/Character.cs
@@ -5,8 +5,8 @@ public class Character : MonoBehaviour {
 
     public float movementSpeed = 5.00f;
     public float mouseSensitivity = 5.00f;
-    float verticalRotation = 0f; //will help with the vertical rotation of the camera
-    public float upDownRate = 60.0f;//sets the rate that the camera will max go to best is 80.
+    float verticalRotation = 0f; //will help with the vertical rotation of the camera 
+    public float upDownRate = 60.0f;//sets the rate that the camera will max go to best is 80. 
 
 	// Use this for initialization  
 	void Start () {
